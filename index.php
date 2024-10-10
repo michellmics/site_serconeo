ff3acc111f1910125a8fd7cb1fe72be3bce582a7
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-10 23:05:38 +0000

    add recaptcha

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $token = $_POST['g-recaptcha-response'];
+    $secretKey = "6LcZ9F0qAAAAAFDgXIwRehRWHU890DwqzZrWYz5Q"; // Substitua pela sua chave secreta
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secretKey}&response={$token}");
+    $responseKeys = json_decode($response, true);
+
+    if(intval($responseKeys["success"]) !== 1) {
+        echo "Por favor, complete o captcha.";
+    } else {
+        // O captcha foi validado com sucesso, processe o formulário.
+        echo "Formulário enviado com sucesso.";
+    }
+}
+die();
+?>
+
 <!doctype html>
 <html class="no-js" lang="zxx">
     <head>
