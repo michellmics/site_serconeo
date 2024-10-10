7e4261036f91978b9d2da114432831fa81ca9b1a
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-10 21:01:23 +0000

    Alteracao da secao de passos da contabilidade

--- index.php
+++ index.php
@@ -263,9 +263,9 @@
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="section-title">
-							<h2>We Are Always Ready to Help You & Your Family</h2>
+							<h2>Uma grande parceria para um resultado de sucesso</h2>
 							<img src="img/section-img.png" alt="#">
-							<p>Lorem ipsum dolor sit amet consectetur adipiscing elit praesent aliquet. pretiumts</p>
+							<p>Descubra os passos essenciais para alcançar o sucesso e conte com nosso apoio especializado em cada etapa do seu percurso.</p>
 						</div>
 					</div>
 				</div>
