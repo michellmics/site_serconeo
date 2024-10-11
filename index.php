547efb5b33928b1a359b0514e7d3a699a5f8ba8b
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-11 02:50:59 +0000

    add recaptcha

--- index.php
+++ index.php
@@ -239,11 +239,10 @@
 										<i class="fa fa-ambulance"></i>
 									</div>
 									<div class="single-content">
-										<span>Moedas Estrangeiras</span>
-										<h4>Cotações</h4>
+										<span>Cotações</span>
+										<h4>Moedas Estrangeiras</h4>
 										<div id="taxas-cambio"></div>
 										<!--  <p>Lorem ipsum sit amet consectetur adipiscing elit. Vivamus et erat in lacus convallis sodales.</p> -->
-										<a href="#">LEARN MORE<i class="fa fa-long-arrow-right"></i></a>
 									</div>
 								</div>
 							</div>
