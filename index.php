0e92fe92795611ea4ecdd7fb5ad83502b9b35324
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-11 02:23:58 +0000

    add recaptcha

--- index.php
+++ index.php
@@ -55,15 +55,13 @@
         <div class="loader-inner"></div>
 
         <div class="indicator"> 
-            <!-- Substitua este SVG pelo seu novo ícone -->
-            <svg width="50px" height="50px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="50" cy="50" r="45" stroke="#000" stroke-width="5" fill="none" />
-                <circle cx="50" cy="50" r="40" stroke="#3498db" stroke-width="5" fill="none" />
-            </svg>
+            <!-- Substitua o SVG pelo seu novo ícone IcoFont -->
+            <i class="icofont-medical-sign-alt" style="font-size: 50px; color: #3498db;"></i>
         </div>
     </div>
 </div>
 <!-- End Preloader -->
+
 
 		<!-- Header Area -->
 		<header class="header" >
