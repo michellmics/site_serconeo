f38b1caa8276ddb3428be8e0b1595350c157b6df
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-11 02:21:33 +0000

    add recaptcha

--- index.php
+++ index.php
@@ -49,20 +49,21 @@
     <body>
 
 		<!-- Preloader -->
-        <div class="preloader">
-            <div class="loader">
-                <div class="loader-outter"></div>
-                <div class="loader-inner"></div>
+<div class="preloader">
+    <div class="loader">
+        <div class="loader-outter"></div>
+        <div class="loader-inner"></div>
 
-                <div class="indicator"> 
-                    <svg width="16px" height="12px">
-                        <polyline id="back" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
-                        <polyline id="front" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
-                    </svg>
-                </div>
-            </div>
+        <div class="indicator"> 
+            <!-- Substitua este SVG pelo seu novo ícone -->
+            <svg width="50px" height="50px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="50" cy="50" r="45" stroke="#000" stroke-width="5" fill="none" />
+                <circle cx="50" cy="50" r="40" stroke="#3498db" stroke-width="5" fill="none" />
+            </svg>
         </div>
-        <!-- End Preloader -->
+    </div>
+</div>
+<!-- End Preloader -->
 
 		<!-- Header Area -->
 		<header class="header" >
