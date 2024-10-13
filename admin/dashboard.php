cc6ed6f56f13c71ebb899ecd76308e3b451ac277
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 16:20:16 +0000

    verificando variavel de sessao

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -309,7 +309,7 @@
           <!-- Sidebar user panel -->
           <div class="user-panel">
             <div class="pull-left image">
-              <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
+              <img src=<? echo $imgProfile ?> class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
               <p><?php echo htmlspecialchars($siteAdmin->ARRAY_USERINFO["USA_NMNOME"]); ?></p>
