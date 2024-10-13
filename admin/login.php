9c8194c18a607dcbfd51663c68c6eb4cbaef7c79
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 12:22:32 +0000

    alter login e registro

--- admin/login.php
+++ admin/login.php
@@ -27,7 +27,7 @@ class LoginSystem extends SITE_ADMIN
                 header("Location: dashboard.php"); // Redireciona após login bem-sucedido
                 exit();
             } else {
-                echo "Usuário ou senha incorretos."; echo $password; echo "-"; echo $user['USA_DCSENHA']; 
+                echo "Usuário ou senha incorretos."; echo $password; echo "-"; echo $user['USA_DCSENHA'];  
             }
         } catch (PDOException $e) {  
             echo "Erro: " . $e->getMessage();
