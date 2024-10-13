f7b0f78ef27dddfc331c5cdd640dd6d74327454e
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 02:32:58 +0000

    alter dashboard admin

--- admin/objetos.php
+++ admin/objetos.php
@@ -1,12 +1,29 @@
 <?php
 
-    include_once 'db.php';
+    //include_once 'db.php';
 
 
 	class SITE_ADMIN
 	{
         //declaração de variaveis 
+        public $pdo;
         public $ARRAY_SITEINFO;
+
+
+        function conexao()
+        {
+            $host = 'localhost';
+            $dbname = 'serconeo_dbprod';
+            $user = 'serconeo_dbprod';
+            $pass = 'dbprodserconeo';
+
+            try {
+                $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Erro na conexão: " . $e->getMessage());
+            }
+        }
 
 
         public function stmtToArray($stmtFunction)
@@ -32,7 +49,9 @@
                                 SBI_DTRENEW_REGISTER_DOMAIN,
                                 SBI_STSITE
                                 FROM SBI_SITEBASEINFO";
-                $stmt = $pdo->prepare($sql);
+
+                $this->conexao();
+                $stmt = $this->pdo->prepare($sql);
                 $stmt->execute();
                 $result = $stmt->fetch(PDO::FETCH_ASSOC);
             } catch (PDOException $e) {
