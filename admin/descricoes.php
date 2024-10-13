6dfbdbb93aeb8bfd117e83d7544a65a473408fb4
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 18:16:48 +0000

    alter descricoes.php

--- admin/descricoes.php
+++ admin/descricoes.php
@@ -29,7 +29,7 @@
      
 
       <!-- Right side column. Contains the navbar and content of the page -->
-      <div class="content-wrapper" style="margin-left: 180px; background-color: white;">
+      <div class="content-wrapper" style="margin-left: 18px; background-color: white;">
         <!-- Content Header (Page header) -->
       
 
