f8f10e947040c1ae7ff668cbdf587e2ee096f572
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 01:40:16 +0000

    alter index  admin

--- admin/index.php
+++ admin/index.php
@@ -53,7 +53,7 @@
         <div class="social-auth-links text-center">
           <p>- OR -</p>
           <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
-          <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
+          <a href="dashboard.php" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
         </div><!-- /.social-auth-links -->
 
         <a href="#">I forgot my password</a><br>
