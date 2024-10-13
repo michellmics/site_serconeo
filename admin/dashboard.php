2ea5519b2c51dbd8d8b39c1aba0978d03e17d69c
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 18:18:05 +0000

    alter descricoes.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -376,7 +376,7 @@
           <iframe 
             src="descricoes.php" 
             class="embed-responsive-item" 
-            style="border:0; width:100%; height:600px;" 
+            style="border:0; width:100%; height:100%;" 
             allowfullscreen>
           </iframe>
         </div>
