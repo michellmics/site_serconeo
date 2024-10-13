3e43bef380c85ef81c1ca819fb8fd1827d49ddd4
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 17:29:34 +0000

    alter descricoes.php

--- admin/descricoes.php
+++ admin/descricoes.php
@@ -26,7 +26,27 @@
   <body class="skin-blue">
    
       
-     
+     <!-- radio -->
+     <div class="form-group">
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                          Option one is this and that&mdash;be sure to include why it's great
+                        </label>
+                      </div>
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+                          Option two can be something else and selecting it will deselect option one
+                        </label>
+                      </div>
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled/>
+                          Option three is disabled
+                        </label>
+                      </div>
+                    </div>
 
       
       <footer class="main-footer">
