2df759cd6beecc97beb5acc1324e86c8ab70dd90
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 01:50:23 +0000

    alter dashboard admin

---
+++ admin/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'serconeo_dbprod';
+$user = 'dbprod';
+$pass = 'dbprodserconeo';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+?>
