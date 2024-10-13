b7237e73a80a0d968577ba8828cc86d0115ab19f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 03:09:10 +0000

    alter dashboard admin

--- admin/register.php
+++ admin/register.php
@@ -31,49 +31,43 @@
   <body class="register-page">
     <div class="register-box">
       <div class="register-logo">
-        <a href="index2.html"><b><?php echo htmlspecialchars($siteAdmin->ARRAY_SITEINFO["SBI_DCSITE"]); ?></b></a>
+        <a href="index2.html"><b><?php echo htmlspecialchars($siteAdmin->ARRAY_SITEINFO["SBI_DCDOMAINSITE"]); ?></b></a>
       </div>
 
       <div class="register-box-body">
-        <p class="login-box-msg">Register a new membership</p>
+        <p class="login-box-msg">Cadastro de novo usuário(a)</p>
         <form action="index.html" method="post">
           <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Full name"/>
+            <input type="text" class="form-control" placeholder="Nome Completo"/>
             <span class="glyphicon glyphicon-user form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Email"/>
+            <input type="text" class="form-control" placeholder="E-mail"/>
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password"/>
+            <input type="password" class="form-control" placeholder="Senha"/>
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Retype password"/>
+            <input type="password" class="form-control" placeholder="Digite a senha novamente"/>
             <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
           </div>
           <div class="row">
             <div class="col-xs-8">    
               <div class="checkbox icheck">
-                <label>
-                  <input type="checkbox"> I agree to the <a href="#">terms</a>
-                </label>
+                
               </div>                        
             </div><!-- /.col -->
             <div class="col-xs-4">
-              <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Registrar</button>
             </div><!-- /.col -->
           </div>
         </form>        
 
-        <div class="social-auth-links text-center">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign up using Facebook</a>
-          <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign up using Google+</a>
-        </div>
+       
 
-        <a href="login.html" class="text-center">I already have a membership</a>
+        <a href="login.html" class="text-center">Já sou um usuário</a>
       </div><!-- /.form-box -->
     </div><!-- /.register-box -->
 
