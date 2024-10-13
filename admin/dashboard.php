facb31721cb0301d570fd1c772969b1193679b0a
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 16:51:24 +0000

    verificando variavel de sessao

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -310,16 +310,7 @@
           <div class="user-panel">
             
           </div>
-          <!-- search form -->
-          <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-              <input type="text" name="q" class="form-control" placeholder="Search..."/>
-              <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
-              </span>
-            </div>
-          </form>
-          <!-- /.search form -->
+         
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
             <li class="header"><?php echo htmlspecialchars($siteAdmin->ARRAY_SITEINFO["SBI_DCDOMAINSITE"]); ?></li>
