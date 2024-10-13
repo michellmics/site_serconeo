69e21ea844c39e2a118424eecba2ab1b2565f713
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 15:48:52 +0000

    verificando variavel de sessao

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,6 +1,6 @@
 <?php
   include_once 'objetos.php';
-  /*
+  
   session_start(); 
   define('SESSION_TIMEOUT', 1800); // 30 minutos
   if (!isset($_SESSION['user_id'])) 
@@ -8,7 +8,7 @@
     header("Location: index.php");
     exit();
   }
-*/
+
   $siteAdmin = new SITE_ADMIN();
   $siteAdmin->getSiteInfo(); 
   $siteAdmin->getUserInfo();
