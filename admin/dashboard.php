dd34f62113c251ee8034e59ee8f2024e11923292
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-13 16:09:04 +0000

    verificando variavel de sessao

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -253,13 +253,13 @@
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img src="dist/img/avatar5.jpg" class="user-image" alt="User Image"/>
+                  <img src="dist/img/avatar5.png" class="user-image" alt="User Image"/>
                   <span class="hidden-xs"><?php echo htmlspecialchars($siteAdmin->ARRAY_USERINFO["USA_NMNOME"]); ?></span>
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
                   <li class="user-header">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
+                    <img src="dist/img/avatar5.png" class="img-circle" alt="User Image" />
                     <p>
                     <?php echo htmlspecialchars($siteAdmin->ARRAY_USERINFO["USA_NMNOME"]); ?> - ADM
                       <small><?php echo htmlspecialchars($siteAdmin->ARRAY_USERINFO["USA_DCEMAIL"]); ?></small>
